8c9a8ad697e45dcad25e521073c91c44a4669abd
Nurfaidzy Riyamizard Saragih <nurfaidzy@gmail.com>
2022-10-26 22:38:47 +0700

    Move page auto direction

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Custom404() {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("pindah kau");
+      router.push("/");
+    }, 2000);
+  }, []);
   return <div>kozong bro</div>;
 }
