40e602489013c4b258b79cd2a35b32a16f5fa629
Nurfaidzy Riyamizard Saragih <nurfaidzy@gmail.com>
2022-10-26 20:56:41 +0700

    using a Link from Next

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <a href="/Homepage">pindah</a>
-      {/* catatan : tinggal paging params dan disesuaikan dengan kebutuhan */}
+      {/* <a href="/Homepage">pindah</a> */}
+      <Link href="/Homepage">Pindah Homepage</Link>
+      {/* catatan : tinggal paging params dan disesuaikan dengan kebutuhan but using Link */}
       <h1>learning next js</h1>
     </div>
   );
